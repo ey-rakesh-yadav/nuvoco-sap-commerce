d194d529cd51be8c3aa706d4416cfab44e9eaa22
Naresh Dahifale <Naresh.Dahifale@in.ey.com>
2023-09-07 15:59:10 +0530

    code setup commit for B2B v2211

--- core-customize/nuvoco/nuvocofulfilmentprocess/gensrc/com/nuvoco/fulfilmentprocess/constants/GeneratedNuvocoFulfilmentProcessConstants.java
+++ core-customize/nuvoco/nuvocofulfilmentprocess/gensrc/com/nuvoco/fulfilmentprocess/constants/GeneratedNuvocoFulfilmentProcessConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 04-Sep-2023, 1:05:35 pm                     ---
+ * --- Generated at 07-Sep-2023, 12:54:32 pm                    ---
  * ----------------------------------------------------------------
  */
 package com.nuvoco.fulfilmentprocess.constants;
