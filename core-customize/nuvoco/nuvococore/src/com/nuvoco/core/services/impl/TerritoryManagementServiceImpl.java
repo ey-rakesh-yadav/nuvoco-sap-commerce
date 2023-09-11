01a7b412a0bf6e06dc25006cd13d0d4c790b4577
Naresh Dahifale <Naresh.Dahifale@in.ey.com>
2023-09-11 14:10:39 +0530

    Nuvoco Catalog impexes

---
+++ core-customize/nuvoco/nuvococore/src/com/nuvoco/core/services/impl/TerritoryManagementServiceImpl.java
@@ -0,0 +1,6 @@
+package com.nuvoco.core.services.impl;
+
+import com.nuvoco.core.services.TerritoryManagementService;
+
+public class TerritoryManagementServiceImpl implements TerritoryManagementService {
+}
